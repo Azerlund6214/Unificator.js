c675f5ab5d3c130c176a771b84d684771a2e4c4f
Azerlund6214 <azerlund6214@gmail.com>
2023-01-11 04:58:38 +0300

    1

--- Unificator.js
+++ Unificator.js
@@ -112,11 +112,32 @@ function str_trimRecursive( str )
 function str_contains(haystack,needle){ return ( haystack.indexOf(needle) > -1 ); }
 function str_replaceOnce(search,replace,subject){ return subject.replace(search, replace); }  // !!!! Только первое вождение
 function str_replaceALL(search,replace,subject){ while( subject.indexOf(search) >= 0 ) { subject = subject.replace(search, replace); } return subject; }
+function str_explode(separator,string){ return string.split(separator); }
+function str_implode(haystack,glue){ return haystack.join(glue); }
 
 
+// ### ### ### ### ### ### ###
+// **/  Кодировщики и тп   \**
+function base64_decodeEngRus( data ) {
+    function utf8_for_base64 (utftext)
+    {
+        var string = ""; var i = 0; var c = c1 = c2 = 0; while ( i < utftext.length ) { c = utftext.charCodeAt(i);
+        if (c < 128) { string += String.fromCharCode(c); i++; } else if((c > 191) && (c < 224)) { c2 = utftext.charCodeAt(i+1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63)); i += 2; } else { c2 = utftext.charCodeAt(i+1); c3 = utftext.charCodeAt(i+2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)); i += 3; } } return string;
+    }
 
-
-
+    var b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";  var o1, o2, o3, h1, h2, h3, h4, bits, i=0, enc='';
+    do {  // https://javascript.ru/php/base64_decode
+        h1 = b64.indexOf(data.charAt(i++)); h2 = b64.indexOf(data.charAt(i++)); h3 = b64.indexOf(data.charAt(i++)); h4 = b64.indexOf(data.charAt(i++));
+        bits = h1<<18 | h2<<12 | h3<<6 | h4;     o1 = bits>>16 & 0xff; o2 = bits>>8 & 0xff; o3 = bits & 0xff;
+        if (h3 == 64)	  enc += String.fromCharCode(o1); else if (h4 == 64) enc += String.fromCharCode(o1, o2); else enc += String.fromCharCode(o1, o2, o3);
+    } while (i < data.length);
+    return utf8_for_base64(enc);  // Решит косяки с рус
+}
+function base64_encodeEngOnly( text ){ return btoa(text); } // Норм для англ
+function base64_decodeEngOnly( text ){ return atob(text); }
+// MD5
 
 
 // ### ### ### ### ### ### ###
