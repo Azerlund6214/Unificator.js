0479809e14f2912e20bdde576d2571ccdc44b094
Egor Karpushin <67274297+Azerlund6214@users.noreply.github.com>
2023-11-09 03:06:27 +0300

    VK_GroupStatPerform()  итого

--- Unificator.js
+++ Unificator.js
@@ -336,9 +336,11 @@ function VK_AlbumsHrefParcer(  ){
 }
 //VK_AlbumsHrefParcer();
 
+
 function VK_GroupStatPerform()
 {
-    var DEBUG = true;
+    // Написан в дек22.  Фулл переписан 091123.
+    var DEBUG = false;
     
     var ROWS = tag_getAllOrFalse(".paginated_table_row");
     
@@ -349,48 +351,50 @@ function VK_GroupStatPerform()
         var rHtml = r.innerHTML;
         if(DEBUG){ logOneRed('====1'); log(rText,rHtml); }
         
-        
         var viewsRawText_1 = PHP_explode('\n\t\n',rText)[1];
         var viewsRawText_2 = PHP_explode('\nчеловек\n\t',viewsRawText_1)[0];
         var viewsRawText_3 = PHP_explode(" / ",viewsRawText_2);
         if(DEBUG){ logOneRed('====2'); log(viewsRawText_1,viewsRawText_2,viewsRawText_3); }
-        
         
         var viewTotal = viewsRawText_3[0];  viewTotal = PHP_str_replace(' ','', viewTotal); // Для цифр больше 1000.
         var viewViral = viewsRawText_3[1];  viewViral = PHP_str_replace(' ','', viewViral);
         var viewSubs  = viewTotal - viewViral;
         if(DEBUG){ logOneRed('====3'); log(viewTotal,viewViral,viewSubs); }
         
-        
-        
-    
-    
-        var views = viewTotal;
-        
-        
-        //var views = PHP_explode(' / ',   PHP_explode('\n\t\n',rText)[1]   )[0];
-        
-        //    views = PHP_str_replace(' ','', views); // Для цифр больше 1000.
-        
-        //log(rText,rHtml,PHP_explode('\n\t\n',rText),views);
-        
+        var viewViral_Perc = 100 - num_percent( viewViral , viewTotal);
+        var viewSubs_Perc  = 100 - num_percent( viewSubs  , viewTotal);
+        if(DEBUG){ logOneRed('====4'); log(viewViral_Perc,viewSubs_Perc); }
         
         var likes = PHP_explode('\t',PHP_explode('\nчеловек\n\t',rText)[1])[0];
-        var percent = Math.floor((likes/views)*100);
-        
+        var percentLikes_vTotal = Math.floor((likes/viewTotal)*100);
         
         var dop = '';
-        if(percent >= 15) dop = ' => ###';
-        if(percent >= 20) dop = ' => ### ###';
-        if(percent >= 25) dop = ' => ### ### ###';
+        if(percentLikes_vTotal >= 10) dop = ' <span style="color:lime;"  >#</span>';
+        if(percentLikes_vTotal >= 15) dop = ' <span style="color:yellow;">##</span>';
+        if(percentLikes_vTotal >= 20) dop = ' <span style="color:orange;">###</span>';
+        if(percentLikes_vTotal >= 25) dop = ' <span style="color:red;"   >####</span>';
         
-        //rHtml = PHP_str_replace('</div>"></div>','</div>">V</div>',rHtml);
-        rHtml = PHP_str_replace('человек','#  '+percent+'  #'+dop,rHtml);
+        var dopViral = '';
+        if(viewViral_Perc >= 25) dopViral = '<span style="color:lime;"  >##</span>';
+        if(viewViral_Perc >= 50) dopViral = '<span style="color:yellow;">####</span>';
+        if(viewViral_Perc >= 75) dopViral = '<span style="color:red;"   >######</span>';
+        
+        var textFinLike = '[ '+percentLikes_vTotal+'% | '+likes+' ]'+dop;
+        var likesPattern = '_2" class="paginated_table_cell posts_reach_td_num pt_align_right" style="white-space: nowrap;">';
+        rHtml = PHP_str_replace( likesPattern+likes , likesPattern+textFinLike , rHtml);
+        
+        var textFinSub = '[SUB='+viewSubs_Perc+'% | VIR='+viewViral_Perc+'% | '+dopViral+' ]';
+        rHtml = PHP_str_replace('человек',textFinSub,rHtml);
+        
+        if(DEBUG){ logOneRed('====5'); log(rHtml,textFinLike); }
+        
         r.innerHTML = rHtml;
-        
         if(DEBUG){ return; }
     }
     
+    // Вк не дочистил код от старой фичи.
+    // class="stat_group_postsreach__evegreen_mark"
+    // https://vk.com/@adminsclub-evergreen
 }
 VK_GroupStatPerform();
 
